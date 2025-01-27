95f7090b70e1922649ab1a1b439b4cd40e270836
Dawsonkw <dawsonkw@gmail.com>
2025-01-27 11:40:24 -0700

    was trying to install tailwind and everything was broken. deleted the original files since they were empty anyway and recreated the project with the same name. added in tailwind.

---
+++ frontend/bookclubAI/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-amber-500 underline text-6xl ">BookClub AI</h1>
+
+      <p>
+        {" "}
+        An AI book club companion. Talk to the ai companion about your book{" "}
+      </p>
+      <p> Coming soon! </p>
+    </>
+  );
+}
+
+export default App;
