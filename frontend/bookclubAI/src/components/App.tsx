ba14758c5f2d353b9a4ab43fadc1ec8b51197910
Dawsonkw <dawsonkw@gmail.com>
2025-01-27 11:53:43 -0700

    added some component files in, moved aroud structure a bit and deleted some unused files

--- frontend/bookclubAI/src/components/App.tsx
+++ frontend/bookclubAI/src/components/App.tsx
@@ -1,14 +1,11 @@
-import "./App.css";
+import "../App.css";
 
 function App() {
   return (
     <>
       <h1 className="text-amber-500 underline text-6xl ">BookClub AI</h1>
 
-      <p>
-        {" "}
-        An AI book club companion. Talk to the ai companion about your book{" "}
-      </p>
+      <p>An AI book club companion. Talk to the ai companion about your book</p>
       <p> Coming soon! </p>
     </>
   );
